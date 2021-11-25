e65f8e01a20044052eed8df1d3d2e960de9e0b82
Francesca Pompi <francesca.pompi93@gmail.com>
2021-11-25 14:25:25 +0100

    codice random

--- src/it/shop/Prodotto.java
+++ src/it/shop/Prodotto.java
@@ -1,5 +1,25 @@
 package it.shop;
 
+import java.util.Random;
+
 public class Prodotto {
+	
+	// attributi
+	private int codice;
+	private String nome;
+	private String descrizione;
+	private double prezzo;
+	private int iva;
+	
+	// costruttori
+	public Prodotto() {
+		this.codice = generaCodice();
+	}
+	
+	// metodo per generare un codice random
+	private int generaCodice() {
+		Random random = new Random();
+		return random.nextInt();
+	}
 
 }
