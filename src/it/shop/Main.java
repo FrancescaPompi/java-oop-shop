e19dec72468ed27e2b2bdb8e3f82e180e3b030af
Francesca Pompi <francesca.pompi93@gmail.com>
2021-11-25 16:35:38 +0100

    testing

--- src/it/shop/Main.java
+++ src/it/shop/Main.java
@@ -1,18 +1,24 @@
 package it.shop;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		// creazione nuovo prodotto
-		Prodotto prodotto1 = new Prodotto();
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Inserisci nome prodotto: ");
+		String nome = scan.nextLine();
+		System.out.print("Inserisci la descrizione del prodotto: ");
+		String descrizione = scan.nextLine();
 		
-		System.out.println("Prodotto: " + prodotto1.nomeEsteso());
+		Prodotto prodotto1 = new Prodotto(nome, descrizione, 0, 0.22);
+		System.out.println("Il prezzo è: " + prodotto1.getPrezzo() + " €");
+		System.out.println("Il prezzo compreso di iva al " + prodotto1.getIva() + " è: " + prodotto1.prezzoCompresaIva() + " €");
 		
-		System.out.println("Il prezzo di questo prodotto è: " + prodotto1.prezzoBase() + ", con iva è: " + prodotto1.prezzoCompresaIva());
 		
-		
-		
+		scan.close();
+
 	}
 
 }
--- src/it/shop/Prodotto.java
+++ src/it/shop/Prodotto.java
@@ -9,16 +9,16 @@ public class Prodotto {
 	private String nome;
 	private String descrizione;
 	private double prezzo;
-	private int iva;
+	private double iva;
 	
 	
 	// costruttori
-	public Prodotto() {
+	public Prodotto(String nome, String descrizione, double prezzo, double iva) {
 		this.codice = generaCodice();
-//		this.nome = nome;
-//		this.descrizione = descrizione;
-//		this.prezzo = prezzo;
-//		this.iva = iva;
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.prezzo = generaPrezzo();
+		this.iva = 0.22;
 	}
 	
 	
@@ -33,7 +33,11 @@ public class Prodotto {
 
 
 	public void setNome(String nome) {
-		this.nome = nome;
+		if(isNomeNullOrEmpty(nome)) {
+			System.out.println("Il nome non può essere vuoto.");
+		} else {
+			this.nome = nome;
+		}
 	}
 
 
@@ -43,7 +47,11 @@ public class Prodotto {
 
 
 	public void setDescrizione(String descrizione) {
-		this.descrizione = descrizione;
+		if(isDescrizioneNullOrEmpty(descrizione)) {
+			System.out.println("La descrizione non può essere vuota.");
+		} else {
+			this.descrizione = descrizione;
+		}
 	}
 
 
@@ -57,35 +65,52 @@ public class Prodotto {
 	}
 
 
-	public int getIva() {
+	public double getIva() {
 		return iva;
 	}
 
 
-	public void setIva(int iva) {
+	public void setIva(double iva) {
 		this.iva = iva;
 	}
 
 
+	
 	// metodo per generare un codice random
 	private int generaCodice() {
 		Random random = new Random();
 		return random.nextInt();
 	}
 	
+	// metodo per generare prezzo random
+	private double generaPrezzo() {
+		Random random = new Random();
+		return random.nextDouble() * 1000;
+	}
+	
 	// metodo per il prezzo base
 	public double prezzoBase() {
-		return prezzo;
+		return this.prezzo;
 	}
 	
 	// metodo per il prezzo con iva
 	public double prezzoCompresaIva() {
-		return prezzo + (prezzo * 0.22);
+		return this.prezzo + (this.prezzo * this.iva);
 	}
 	
 	// metodo per il nome esteso
 	public String nomeEsteso() {
-		return codice + ",  " + nome;
+		return this.codice + ",  " + this.nome;
+	}
+	
+	// metodo per validare che il nome non sia null o lunghezza 0
+	private boolean isNomeNullOrEmpty(String nome) {
+		return nome == null || nome.length() == 0;
+	}
+	
+	// metodo per validare la descrizione
+	private boolean isDescrizioneNullOrEmpty(String descrizione) {
+		return descrizione == null || descrizione.length() == 0;
 	}
 	
 
