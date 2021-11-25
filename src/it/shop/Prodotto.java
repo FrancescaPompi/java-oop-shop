a730cc850f8bd980f18df3c4e4ce26beb42e3042
Francesca Pompi <francesca.pompi93@gmail.com>
2021-11-25 14:31:00 +0100

    metodi getter e setter

--- src/it/shop/Prodotto.java
+++ src/it/shop/Prodotto.java
@@ -11,15 +11,69 @@ public class Prodotto {
 	private double prezzo;
 	private int iva;
 	
+	
 	// costruttori
 	public Prodotto() {
 		this.codice = generaCodice();
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.prezzo = prezzo;
+		this.iva = iva;
 	}
 	
+	
+	// metodi getter e setter
+	public int getCodice() {
+		return codice;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+
+	public double getPrezzo() {
+		return prezzo;
+	}
+
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+
+	public int getIva() {
+		return iva;
+	}
+
+
+	public void setIva(int iva) {
+		this.iva = iva;
+	}
+
+
 	// metodo per generare un codice random
 	private int generaCodice() {
 		Random random = new Random();
 		return random.nextInt();
 	}
+	
+	
+	
 
 }
